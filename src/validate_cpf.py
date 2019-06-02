f774c714a3952ecab7735e760e3e7e3a7dbdbc85
Gaston Guillaux <gaston.guillaux@gmail.com>
2019-06-02 00:46:39 -0300

    Class CPF improvement

--- src/validate_cpf.py
+++ src/validate_cpf.py
@@ -1,18 +1,39 @@
 class CPF:
     def __init__(self, cpf_number):
         self.cpf = cpf_number
-        self.cpf_clean = self.__clean_cpf()
-        self.first_digit = None
-        self.second_digit = None
+        self.__clean = self.__clean_cpf()
+        self.__proper_size = self.__has_valid_length()
+        self.body = ""
+        self.validation_digits = ""
+        self.first_digit = ""
+        self.second_digit = ""
         self.is_valid = self.__is_valid()
 
+        self.__divide()
+
     def __clean_cpf(self):
-        # TODO function to clean CPF by removing . and - chars
-        return None
+        return ''.join(l for l in self.cpf if l.isdigit())
+    
+    def __has_valid_length(self):
+        if len(self.__clean) == 11:
+            return True
+        else:
+            return False
+
+    def __divide(self):
+        if self.__proper_size:
+            self.body = self.__clean[:9]
+            self.validation_digits = self.__clean[9:]
+            self.first_digit = self.validation_digits[0]
+            self.second_digit = self.validation_digits[1]
 
     def __is_valid(self):
-        return None
+        return False
 
 if __name__ == "__main__":
-    # TODO read txt file with test cases and parse each cpf
-    pass
+    
+    cpf_file_path = r'./test/cpfs.txt'
+    with open(cpf_file_path, 'r') as f:
+        for i, l in enumerate(f.readlines()):
+            cpf = CPF(l)
+            print('{} - \t {}'.format(i, cpf.clean))
