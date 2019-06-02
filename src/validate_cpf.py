ba4670f0667e1d04bba0dbccbfac75f6d4b85925
Gaston Guillaux <gaston.guillaux@gmail.com>
2019-06-01 21:36:03 -0300

    Python code modifications

--- src/validate_cpf.py
+++ src/validate_cpf.py
@@ -1,10 +1,18 @@
 class CPF:
     def __init__(self, cpf_number):
         self.cpf = cpf_number
-        self.cpf_clean = self.clean(cpf)
+        self.cpf_clean = self.__clean_cpf()
+        self.first_digit = None
+        self.second_digit = None
+        self.is_valid = self.__is_valid()
 
     def __clean_cpf(self):
         # TODO function to clean CPF by removing . and - chars
+        return None
+
+    def __is_valid(self):
+        return None
 
 if __name__ == "__main__":
     # TODO read txt file with test cases and parse each cpf
+    pass
